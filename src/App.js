991a0dd814424e25a7d836ad2dddb543570c9975
kellar1995 <kellar_a@yahoo.com>
2021-10-25 15:05:38 -0500

    added features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 import Button from './components/Button'
 
+
+
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  handleClick = (step) => {
+    let newCount = this.state.count + step
+    this.setState({
+        count: newCount
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
+        <div className="container">
+        <h1>Hello {this.props.name}</h1>
+        <Button step ={1} incrementCount={this.handleClick}/>
+        <Button step ={5} incrementCount={this.handleClick}/>
+        <Button step ={10} incrementCount={this.handleClick}/>
+        <Button step ={25} incrementCount={this.handleClick}/>
+        <Button step ={100} incrementCount={this.handleClick}/>
+        <h3>Count is at {this.state.count}</h3>
       </div>
     )
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Button extends Component {
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //       count: 0
+    //     }
+    //   }
+
+    // handleClick = () => {
+    //     let newCount = this.state.count +1
+    //     this.setState({
+    //         count: newCount
+    //     })
+    // }
+    
     render() {
         return (
             <div>
-                <button>Click Me!</button>
+                <button className="btn btn-success mb-2" onClick={() => this.props.incrementCount(this.props.step)}>+{this.props.step}</button>
             </div>
         )
     }
